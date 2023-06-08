e7dbc94b75ef85336bffb2b6819b3915eb24ba8c
prafful026 <prafful@inito.com>
2023-06-08 20:34:20 +0530

    fetch actors from movie detail page & code refactor

--- main.rb
+++ main.rb
@@ -1,58 +1,29 @@
-require "httparty"
-require "nokogiri"
+require_relative 'movie_db.rb'
 
 def is_valid_number_of_movies?(n)
   n > 0 && n <= 250
 end
 
-movie_db = {}
-
-response = HTTParty.get("https://www.imdb.com/chart/top")
-document = Nokogiri::HTML(response.body)
-
 puts "Enter number of top movies to fetch from imdb top 250 movies list"
 
-num_top_movies= gets.chomp.to_i
+num_top_movies = gets.chomp.to_i
 
 unless is_valid_number_of_movies? num_top_movies
   puts "invalid N"
   return
 end
 
-document.css(".lister-list tr").each_with_index do |row_element, index|
+movies_db = MovieDB.new(num_top_movies)
 
-  if index >= num_top_movies
-    break
+take_query = true
+
+while take_query
+  puts "Enter cast name & the number of their top movies to fetch"
+  cast_name = gets.chomp
+  num_top_movies_of_actor = gets.chomp.to_i
+  unless is_valid_number_of_movies? num_top_movies_of_actor
+    puts "invalid number of movies"
+    return
   end
-
-  anchor_element = row_element.css(".titleColumn a")
-  movie_name = anchor_element.text
-  movie_cast = anchor_element.attribute("title").to_s.downcase.split(", ").drop(1)
-  movie_cast.each do |cast|
-    if movie_db[cast].nil?
-      movie_db[cast] = []
-    end
-    movie_db[cast] << movie_name
-  end
+  puts movies_db.query(cast_name, num_top_movies_of_actor).to_s
 end
-
-puts movie_db.to_s
-
-puts "Enter cast name & the number of their top movies to fetch"
-
-cast_name = gets.chomp
-
-num_top_movies_of_actor = gets.chomp.to_i
-
-unless is_valid_number_of_movies? num_top_movies_of_actor
-  puts "invalid M"
-  return
-end
-
-movies_to_output = []
-
-unless movie_db[cast_name].nil?
-  movies_to_output = movie_db[cast_name][0..(num_top_movies_of_actor - 1)]
-end
-
-puts movies_to_output.to_s
---
+++ movie_db.rb
@@ -0,0 +1,50 @@
+require "httparty"
+require "nokogiri"
+
+class MovieDB
+  include HTTParty
+  base_uri 'imdb.com'
+
+  def initialize(num_movies_to_fetch)
+    @movie_actors_db = fetch_movies(num_movies_to_fetch)
+  end
+
+  def query(actor_name, num_top_movies_of_actor)
+    movies_to_output = []
+    actor_name.downcase!
+    unless @movie_actors_db[actor_name].nil?
+      movies_to_output = @movie_actors_db[actor_name][0..(num_top_movies_of_actor - 1)]
+    end
+    movies_to_output
+  end
+
+  private
+
+  def fetch_movies(num_movies_to_fetch)
+    headers = {
+      'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123 Safari/537.36'
+    }
+    movie_actors_db = {}
+    response = self.class.get("/chart/top")
+    document = Nokogiri::HTML(response.body)
+    document.css(".lister-list tr").each_with_index do |row_element, index|
+      if index >= num_movies_to_fetch
+        break
+      end
+      anchor_element = row_element.css(".titleColumn a")
+      movie_name = anchor_element.text
+      movie_detail_page_path = anchor_element.attribute("href")
+      puts "fetching #{movie_name}"
+      movie_detail_html_document = Nokogiri::HTML(self.class.get(movie_detail_page_path, headers: headers).body)
+      movie_detail_html_document.css("a[data-testid=title-cast-item__actor]").each do |cast|
+        actor_name = cast.text.to_s.downcase
+        if movie_actors_db[actor_name].nil?
+          movie_actors_db[actor_name] = []
+        end
+        movie_actors_db[actor_name] << movie_name
+      end
+    end
+    movie_actors_db
+  end
+
+end
