9251f9e7d271c7b8b91c7842afd846d3df012ef8
prafful026 <prafful@inito.com>
2023-06-13 00:57:30 +0530

    fixing movies order

--- movie_db.rb
+++ movie_db.rb
@@ -21,24 +21,21 @@ class MovieDB
 
   private
 
-  def fetch_movie_actors(movie_detail_page_path, movie_name)
-    headers = {
-      'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123 Safari/537.36'
-    }
-    movie_detail_html_document = Nokogiri::HTML(self.class.get(movie_detail_page_path, headers: headers).body)
+  def fetch_movie_actors(movie_detail_page_path, movie_name, movie_rank)
+    movie_detail_html_document = Nokogiri::HTML(self.class.get(movie_detail_page_path, headers: HEADERS).body)
     movie_detail_html_document.css("a[data-testid=title-cast-item__actor]").each do |cast|
       actor_name = cast.text.to_s.downcase
       if @movie_actors_db[actor_name].nil?
         @movie_actors_db[actor_name] = []
       end
-      @movie_actors_db[actor_name] << movie_name
+      @movie_actors_db[actor_name] << { :movie_name => movie_name, :movie_rank => movie_rank }
     end
     puts "fetched #{movie_name}"
   end
 
   def fetch_movies(num_movies_to_fetch)
     threads = []
-    response = self.class.get("/chart/top")
+    response = self.class.get("/chart/top", HEADERS)
     document = Nokogiri::HTML(response.body)
     document.css(".lister-list tr").each_with_index do |row_element, index|
       if index >= num_movies_to_fetch
@@ -47,8 +44,21 @@ class MovieDB
       anchor_element = row_element.css(".titleColumn a")
       movie_name = anchor_element.text
       movie_detail_page_path = anchor_element.attribute("href")
-      threads << Thread.new { fetch_movie_actors(movie_detail_page_path, movie_name) }
+      threads << Thread.new { fetch_movie_actors(movie_detail_page_path, movie_name, index + 1) }
     end
     threads.each(&:join)
+    @movie_actors_db.each do |_, movie_list|
+      movie_list.sort! do |a, b|
+        if b.nil?
+          -1
+        else
+          a[:movie_rank] <=> b[:movie_rank]
+        end
+      end
+    end
   end
+
+  HEADERS = {
+    'User-Agent' => 'MMMozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/519.36 (KHTML, like Gecko) Chrome/123 Safari/137.36'
+  }
 end
